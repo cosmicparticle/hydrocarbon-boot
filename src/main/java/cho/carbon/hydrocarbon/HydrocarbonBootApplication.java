644385c89e18ccbbf264bb10bf8c63d063286f08
cosmicparticle <wnq111@126.com>
2023-06-05 11:03:11 +0800

    Initial commit

---
+++ src/main/java/cho/carbon/hydrocarbon/HydrocarbonBootApplication.java
@@ -0,0 +1,23 @@
+package cho.carbon.hydrocarbon;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+import java.net.UnknownHostException;
+
+@SpringBootApplication(exclude = { RedisAutoConfiguration.class, MongoAutoConfiguration.class, DataSourceAutoConfiguration.class}  )
+public class HydrocarbonBootApplication {
+    static Logger logger = LoggerFactory.getLogger(HydrocarbonBootApplication.class);
+
+    public static void main(String[] args) throws UnknownHostException {
+
+        SpringApplication.run(HydrocarbonBootApplication.class, args);
+
+    }
+
+}
